b57e5694c130eacb0dd7765cf6cbe882831cea6d
kbg1513 <kbgarcia1513@gmail.com>
2025-11-02 02:51:36 +0000

    Added quick email to contact page

--- portfolio/src/components/pages/ContactPage/ContactPage.jsx
+++ portfolio/src/components/pages/ContactPage/ContactPage.jsx
@@ -5,7 +5,143 @@ import { TfiEmail } from "react-icons/tfi";
 import { CiMobile3 } from "react-icons/ci";
 import { FaFacebookF, FaGithub, FaLinkedin } from "react-icons/fa";
 
+
+const inputs = [
+    { 
+        label: "Name",
+        additionalInfo: "",
+        direction: "row",
+        id: "name",
+        placeholder: "Name of Sender",
+        icon: '',
+        value: "",
+        type: "text",
+        required: true,
+        disabled: false,
+        checked: false,
+        pattern: "",
+        rows: "",
+        cols: ""
+    },
+    { 
+        label: "Email",
+        additionalInfo: "",
+        direction: "row",
+        id: "email",
+        placeholder: "Email of Sender",
+        icon: '',
+        value: "",
+        type: "email",
+        required: true,
+        disabled: false,
+        checked: false,
+        pattern: "",
+        rows: "",
+        cols: ""
+    },
+    { 
+        label: "Project Title or Subject",
+        additionalInfo: "",
+        direction: "row",
+        id: "title",
+        placeholder: "Project Title or Subject",
+        icon: '',
+        value: "",
+        type: "text",
+        required: true,
+        disabled: false,
+        checked: false,
+        pattern: "",
+        rows: "",
+        cols: ""
+    },
+    { 
+        uniqueClass: "description-textarea",
+        label: "Project Description or Message",
+        additionalInfo: "",
+        direction: "row",
+        id: "description",
+        placeholder: "Description or message",
+        icon: '',
+        type: "textarea",
+        required: true,
+        disabled: false,
+        checked: false,
+        pattern: "",
+        rows: "20",
+        cols: ""
+    }
+];
+
+const initialContactFormValues = {
+    'name': '',
+    'email': '',
+    'title': '',
+    'description': ''
+}
+
+/* Email JS variables */
+const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const autoReplyTemplate = import.meta.env.VITE_EMAILJS_TEMPLATE_ID_AUTO_REPLY;
+const notifyMeTemplate = import.meta.env.VITE_EMAILJS_TEMPLATE_ID_OWNER_NOTIFICATION;
+const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
 const ContactPage = () => {
+
+    const [contactFormValues,setContactFormValues] = React.useState(initialContactFormValues);
+    const [debouncedContactFormValues, setDebouncedContactFormValues] = React.useState(initialContactFormValues);
+
+    const handleContactFormChange = React.useCallback((e) => {
+        const { keyname } = e.currentTarget.dataset;
+        const currentValue = e.currentTarget.value;
+
+        setContactFormValues(prevData => ({
+            ...prevData,
+            [keyname]: currentValue
+        }));
+    }, []);
+
+    React.useEffect(()=> {
+        const debounceInputTimeout = setTimeout(() => {
+            setDebouncedContactFormValues(contactFormValues);
+        }, 500);
+        return () => clearTimeout(debounceInputTimeout);
+    }, [contactFormValues]);
+
+    const contactFormInputs = React.useMemo(() => {
+        return inputs.map((input, index) => ({
+            ...input,
+            onchange: handleContactFormChange,
+            value: contactFormValues[input.id],
+            dataAttributes: {
+                "data-index": index,
+                "data-keyname": input.id,
+            }
+        }));
+    },[contactFormValues]);
+
+    const handleFormSubmit = React.useCallback((e) => {
+        e.preventDefault();
+        //Send notification to own email
+        emailjs.send(serviceId, notifyMeTemplate, contactFormValues, publicKey)
+        .then((result) => {
+            console.log(result.text);
+            alert('Email sent to kbg successfully')
+        }, (error) => {
+            alert(`Notification to kbg error: ${error.text}`)
+        });
+
+        //Send auto-reply to sender of contact/query
+        emailjs.send(serviceId, autoReplyTemplate, contactFormValues, publicKey)
+        .then((result) => {
+            console.log(result.text);
+            alert('Email sent to kbg successfully')
+        }, (error) => {
+            alert(`Notification to kbg error: ${error.text}`)
+        });
+
+        setContactFormValues(initialContactFormValues);
+    },[]);
 
     const contacts = [
         {icon: <TfiEmail/>, name: 'Email', content: 'kbgarcia8@gmail.com', link: 'mailto:kbgarcia8@gmail.com'},
@@ -33,6 +169,10 @@ const ContactPage = () => {
                     ))}
                 </styled.ContactSpace>
             </styled.ContactSection>
+            <styled.QuickContactSection title={'Quick Email'} description={`You can also contact me right away by sending me the details below`}>
+                <Divider/>
+                <styled.QuickContactForm fieldsetHeight={'60vh'} inputClassName={'contact-me-form-inputs'} id={'contact-me'} formInputs={contactFormInputs} labelAndInputContainerClass={'contact-label-input-container'} hasSubmit handleSubmit={handleFormSubmit}/>
+            </styled.QuickContactSection>
         </styled.ContactPageWrapper>
     )
 }
--- portfolio/src/components/pages/ContactPage/ContactPage.styles.js
+++ portfolio/src/components/pages/ContactPage/ContactPage.styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { v } from "styles/variables.js";
 import Section from "components/molecules/Section";
+import Form from "components/organisms/Form";
 import { Link } from "react-router-dom";
 
 export const ContactPageWrapper = styled.div`
@@ -12,7 +13,7 @@ export const ContactPageWrapper = styled.div`
 
 export const ContactSection = styled(Section)`
     width: 100%;
-    height: 75vh;
+    height: 55vh;
 
     & h3 {
         font-size: ${v.fontSize.medium};
@@ -25,7 +26,7 @@ export const ContactSpace = styled.div`
     width: 100%;
     display: grid;
     grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(3, 12.5%);
+    grid-template-rows: repeat(3, 25%);
     gap: ${v.spacing.medium};
 `;
 
@@ -74,4 +75,69 @@ export const Information = styled(InformationTitle)`
     font-family: ${v.fonts.primary};
     font-weight: ${v.fontWeight.light};
     font-size: ${v.fontSize.small};
+`;
+
+export const QuickContactSection = styled(Section)`
+    width: 100%;
+    height: 75vh;
+`;
+
+export const QuickContactForm = styled(Form)`
+    width: 100%;
+
+    .contact-label-input-container{
+        width: 85%;
+        height: 15%;
+        padding-left: ${v.spacing.medium};
+    }
+
+    & label {
+        align-items: flex-start;
+    }
+    
+    .contact-me-form-inputs {
+        width: 85%;
+        height: 100%;
+        font-size: ${v.fontSize.small};
+        border: none;
+        outline: none;
+        background-color: ${({theme}) => theme.bg};
+        border-bottom: ${v.borderThickness.medium} solid ${({theme}) => theme.text};
+    }
+
+    .contact-me-form-inputs:hover{
+        border-bottom: ${v.borderThickness.medium} solid ${({theme}) => theme.blue2};
+    }
+
+    .form-fieldset{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        border-radius: ${v.borderRadius.small};
+        border: ${v.borderThickness.light} solid ${({theme})=>theme.text};
+    }
+    .form-fieldset:hover{
+        border-color: ${({theme}) => theme.blue};
+    }
+
+    .description-textarea{
+        height: 25%;
+    }
+    
+    .form-main-button-container{
+        justify-content: center;
+        align-items: center;
+        height: 10%;
+
+        & button {
+            height: 100%;
+            width: 35%;
+            border-radius: ${v.borderRadius.small};
+
+            & span {
+                font-size: ${v.fontSize.small};
+            }
+        }
+    }
 `;
